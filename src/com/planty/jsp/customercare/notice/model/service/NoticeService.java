8ef0db7b23d64de2e95d0ae7fb2cce7f852bc683
최서경 <sg22049@gmail.com>
2022-03-24 21:56:22 +0900

    notice 진행중

--- src/com/planty/jsp/customercare/notice/model/service/NoticeService.java
+++ src/com/planty/jsp/customercare/notice/model/service/NoticeService.java
@@ -28,85 +28,85 @@ public class NoticeService {
 		
 		return noticeList;
 	}
-//	
-//	/* 신규 공지 사항 등록용 메소드 */
-//	public int insertNotice(NoticeDTO newNotice) {
-//		
-//		SqlSession session = getSqlSession();
-//		
-//		int result = noticeDAO.insertNotice(session, newNotice);
-//		
-//		if(result > 0) {
-//			session.commit();
-//		} else {
-//			session.rollback();
-//		}
-//		
-//		session.close();
-//		
-//		return result;
-//	}
-//	
-//	/* 공지사항 상세보기용 메소드 */
-//	public NoticeDTO selectNoticeDetail(int no) {
-//		
-//		SqlSession session = getSqlSession();
-//		NoticeDTO noticeDetail = null;
-//		
-//		int result = noticeDAO.incrementNoticeCount(session, no);
-//		
-//		if(result > 0) {
-//			noticeDetail = noticeDAO.selectNoticeDetail(session, no);
-//			
-//			if(noticeDetail != null) {
-//				session.commit();
-//			} else {
-//				session.rollback();
-//			}
-//		} else {
-//			session.rollback();
-//		}
-//		
-//		session.close();
-//		
-//		return noticeDetail;
-//	}
-//
-//	/* 공지사항 수정용 메소드 */
-//	public int updateNotice(NoticeDTO updateNotice) {
-//		
-//		SqlSession session = getSqlSession();
-//		
-//		int result = noticeDAO.updateNotice(session, updateNotice);
-//		
-//		if(result > 0) {
-//			session.commit();
-//		} else {
-//			session.rollback();
-//		}
-//		
-//		session.close();
-//		
-//		return result;
-//		
-//	}
-//
-//	/* 공지사항 삭제용 메소드 */
-//	public int deleteNotice(int no) {
-//		
-//		SqlSession session = getSqlSession();
-//		
-//		int result = noticeDAO.deleteNotice(session, no);
-//		
-//		if(result > 0) {
-//			session.commit();
-//		} else {
-//			session.rollback();
-//		}
-//		
-//		session.close();
-//		
-//		return result;
-//	}
+	
+	/* 신규 공지 사항 등록용 메소드 */
+	public int insertNotice(NoticeDTO newNotice) {
+		
+		SqlSession session = getSqlSession();
+		
+		int result = noticeDAO.insertNotice(session, newNotice);
+		
+		if(result > 0) {
+			session.commit();
+		} else {
+			session.rollback();
+		}
+		
+		session.close();
+		
+		return result;
+	}
+	
+	/* 공지사항 상세보기용 메소드 */
+	public NoticeDTO selectNoticeDetail(int no) {
+		
+		SqlSession session = getSqlSession();
+		NoticeDTO noticeDetail = null;
+		
+		int result = noticeDAO.incrementNoticeCount(session, no);
+		
+		if(result > 0) {
+			noticeDetail = noticeDAO.selectNoticeDetail(session, no);
+			
+			if(noticeDetail != null) {
+				session.commit();
+			} else {
+				session.rollback();
+			}
+		} else {
+			session.rollback();
+		}
+		
+		session.close();
+		
+		return noticeDetail;
+	}
+
+	/* 공지사항 수정용 메소드 */
+	public int updateNotice(NoticeDTO updateNotice) {
+		
+		SqlSession session = getSqlSession();
+		
+		int result = noticeDAO.updateNotice(session, updateNotice);
+		
+		if(result > 0) {
+			session.commit();
+		} else {
+			session.rollback();
+		}
+		
+		session.close();
+		
+		return result;
+		
+	}
+
+	/* 공지사항 삭제용 메소드 */
+	public int deleteNotice(int no) {
+		
+		SqlSession session = getSqlSession();
+		
+		int result = noticeDAO.deleteNotice(session, no);
+		
+		if(result > 0) {
+			session.commit();
+		} else {
+			session.rollback();
+		}
+		
+		session.close();
+		
+		return result;
+	}
 	
 }
